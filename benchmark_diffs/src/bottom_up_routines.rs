7bc5050ce363f00141e064a3b87af25013a76d35
quentin Le Dilavrec <ledilavrecquentin@gmail.com>
2026-06-11 14:16:07 +0200

    refactoring(diff bench): cleanup routines

--- benchmark_diffs/src/bottom_up_routines.rs
+++ benchmark_diffs/src/bottom_up_routines.rs
@@ -1,3 +1,4 @@
+//! Variants of the bottom-up matchers
 use const_chunks::IteratorConstChunks;
 use hyper_diff::decompressed_tree_store::CompletePostOrder;
 use hyper_diff::decompressed_tree_store::lazy_post_order::LazyPostOrder;
@@ -32,30 +33,29 @@ pub trait WithSetup {
 }
 
 #[repr(transparent)]
-struct Vek<T>(Vec<T>);
-impl<F> std::ops::AddAssign<Routine<F>> for Vek<Routine<F>> {
+struct RVec<T>(Vec<Routine<T>>);
+impl<F> std::ops::AddAssign<Routine<F>> for RVec<F> {
     fn add_assign(&mut self, other: Routine<F>) {
         self.0.push(other);
     }
 }
-type RVec<R> = Vek<Routine<R>>;
 
-impl<HAST, IdN, B: WithSetup> Routines<(HAST, IdN, B)> {
+impl<HAST, IdN, B: WithSetup> Routines<(HAST, IdN, B)>
+where
+    IdN: hyperast::types::UniformNodeId,
+    for<'a> &'a HAST: HyperASTShared<IdN = IdN>,
+    for<'a> &'a HAST: HyperAST + Copy,
+    for<'t, 'a> LendT<'t, &'a HAST>: WithStats,
+    for<'t, 'a> LendT<'t, &'a HAST>: WithHashs,
+    for<'a> <&'a HAST as HyperASTShared>::Label: std::cmp::Eq,
+    for<'t, 'a> LendT<'t, &'a HAST>: WithMetaData<compo::StmtCount>,
+    for<'t, 'a> LendT<'t, &'a HAST>: WithMetaData<compo::MemberImportCount>,
+{
     pub fn lazy_to_complete<const MAX_SIZE: usize, const S: usize>()
-    -> impl Iterator<Item = Routine<fn(&mut B, &HAST, OwnedLazyMapping<IdN, S>)>>
-    where
-        IdN: hyperast::types::UniformNodeId,
-        for<'a> &'a HAST: HyperASTShared<IdN = IdN>,
-        for<'a> &'a HAST: HyperAST + Copy,
-        for<'t, 'a> LendT<'t, &'a HAST>: WithStats,
-        for<'t, 'a> LendT<'t, &'a HAST>: WithHashs,
-        for<'a> <&'a HAST as HyperASTShared>::Label: std::cmp::Eq,
-        for<'t, 'a> LendT<'t, &'a HAST>: WithMetaData<compo::StmtCount>,
-        for<'t, 'a> LendT<'t, &'a HAST>: WithMetaData<compo::MemberImportCount>,
-    {
+    -> impl Iterator<Item = Routine<fn(&mut B, &HAST, OwnedLazyMapping<IdN, S>)>> {
         use hyper_diff::matchers::heuristic::cd;
         use hyper_diff::matchers::heuristic::gt;
-        let mut routines: RVec<fn(&mut B, &HAST, OwnedLazyMapping<IdN, S>)> = Vek(vec![]);
+        let mut routines: RVec<fn(&mut B, &HAST, OwnedLazyMapping<IdN, S>)> = RVec(vec![]);
         macro_rules! routine {
             (@in $prep:ident; $f:expr; $mpr:pat => $matcher:expr) => {
                 routines += Routine { routine: |b, hyperast, (owned, mappings)| { b.run(
@@ -98,10 +98,6 @@ impl<HAST, IdN, B: WithSetup> Routines<(HAST, IdN, B)> {
                                 out[i + 1] = Some(b);
                             });
                         (out, mappings)
-                        // let mapper = Mapper::prep(hyperast, mappings, owned);
-                        // let mapper = mapper.map(CDS::from, CDS::from);
-                        // let mapper = match mapper { $mpr => {$matcher}};
-                        // mapper.mapping.map(|a| a.decomp, |a| a.decomp)
                     },
                 );}, name: $f, prep: Prep::$prep };
             };
@@ -126,59 +122,6 @@ impl<HAST, IdN, B: WithSetup> Routines<(HAST, IdN, B)> {
                 }
             };
         }
-        // routines += Routine {
-        //     routine: |b, hyperast, (owned, mappings)| {
-        //         b.run(
-        //             || (owned.clone(), mappings.clone()),
-        //             |(owned, mut mappings)| {
-        //                 let mut out = std::array::from_fn::<_, S, _>(|_| {
-        //                     MaybeUninit::<CompletePostOrder<_, _>>::uninit()
-        //                 });
-        //                 let mut it = owned.into_iter();
-        //                 let mut tmp = CDS::from(Decompressible {
-        //                     hyperast,
-        //                     decomp: it.next().unwrap(),
-        //                 })
-        //                 .decomp;
-
-        //                 std::mem::swap(unsafe { out[0].assume_init_mut() }, &mut tmp);
-        //                 it.zip(mappings.iter_mut())
-        //                     .enumerate()
-        //                     .for_each(|(i, (b, c))| {
-        //                         let a = std::mem::replace(&mut out[i], MaybeUninit::uninit());
-        //                         let b = CDS::from(Decompressible {
-        //                             hyperast,
-        //                             decomp: b,
-        //                         });
-        //                         let mapper = Mapper {
-        //                             hyperast,
-        //                             mapping: hyper_diff::matchers::Mapping {
-        //                                 src_arena: Decompressible {
-        //                                     hyperast,
-        //                                     decomp: unsafe { a.assume_init() },
-        //                                 },
-        //                                 dst_arena: b,
-        //                                 mappings: c,
-        //                             },
-        //                         };
-        //                         let mapper = gt::greedy_bottom_up_matcher::GreedyBottomUpMatcher::<
-        //                             _,
-        //                             M,
-        //                             MAX_SIZE,
-        //                         >::match_it(mapper);
-        //                         let mut a = MaybeUninit::new(mapper.mapping.src_arena.decomp);
-        //                         std::mem::swap(&mut out[i], &mut a);
-
-        //                         let mut b = MaybeUninit::new(mapper.mapping.dst_arena.decomp);
-        //                         std::mem::swap(&mut out[i + 1], &mut b);
-        //                     });
-        //                 (out, mappings)
-        //             },
-        //         );
-        //     },
-        //     name: "".to_string(),
-        //     prep: Prep::CD,
-        // };
         routine!("Greedy_{}"; [MAX_SIZE]
             mapper => gt::greedy_bottom_up_matcher::
                 GreedyBottomUpMatcher::<_, M, MAX_SIZE>::match_it(mapper));
@@ -221,24 +164,11 @@ impl<HAST, IdN, B: WithSetup> Routines<(HAST, IdN, B)> {
                 ::XYBottomUpMatcher::<_>::match_it(mapper));
         routines.0.into_iter()
     }
-}
-
-impl<HAST, IdN, B: WithSetup> Routines<(HAST, IdN, B)> {
     pub fn lazy_to_lazy<const MAX_SIZE: usize, const S: usize>()
-    -> impl Iterator<Item = Routine<fn(&mut B, &HAST, OwnedLazyMapping<IdN, S>)>>
-    where
-        IdN: hyperast::types::UniformNodeId,
-        for<'a> &'a HAST: HyperASTShared<IdN = IdN>,
-        for<'a> &'a HAST: HyperAST + Copy,
-        for<'t, 'a> LendT<'t, &'a HAST>: WithStats,
-        for<'t, 'a> LendT<'t, &'a HAST>: WithHashs,
-        for<'a> <&'a HAST as HyperASTShared>::Label: std::cmp::Eq,
-        for<'t, 'a> LendT<'t, &'a HAST>: WithMetaData<compo::StmtCount>,
-        for<'t, 'a> LendT<'t, &'a HAST>: WithMetaData<compo::MemberImportCount>,
-    {
+    -> impl Iterator<Item = Routine<fn(&mut B, &HAST, OwnedLazyMapping<IdN, S>)>> {
         use hyper_diff::matchers::heuristic::cd;
         use hyper_diff::matchers::heuristic::gt;
-        let mut routines: RVec<fn(&mut B, &HAST, OwnedLazyMapping<IdN, S>)> = Vek(vec![]);
+        let mut routines: RVec<fn(&mut B, &HAST, OwnedLazyMapping<IdN, S>)> = RVec(vec![]);
         macro_rules! routine {
             (@in $prep:ident; $f:expr; $mpr:pat => $matcher:expr) => {
                 routines += Routine { routine: |b, hyperast, (owned, mappings)| { b.run(
@@ -266,10 +196,6 @@ impl<HAST, IdN, B: WithSetup> Routines<(HAST, IdN, B)> {
                             let _mpr = match mpr { $mpr => {$matcher}};
                         });
                         (owned, mappings)
-                        // let mut mapper = Mapper::prep(hyperast, mappings, owned);
-                        // let mpr = mapper.mut_decompressible();
-                        // let _mpr = match mpr { $mpr => {$matcher}};
-                        // mapper.mapping.map(|a| a.decomp, |a| a.decomp)
                     },
                 );}, name: $f, prep: Prep::$prep};
             };
@@ -294,45 +220,6 @@ impl<HAST, IdN, B: WithSetup> Routines<(HAST, IdN, B)> {
                 }
             };
         }
-        routines +=
-            Routine {
-                routine: |b, hyperast, (owned, mappings)| {
-                    b.run(
-                        || (owned.clone(), mappings.clone()),
-                        |(mut owned, mut mappings)| {
-                            owned
-                                .iter_mut()
-                                .const_chunks::<2>()
-                                .zip(mappings.iter_mut().const_chunks::<2>())
-                                .for_each(|([a, b], [c, _])| {
-                                    let mpr = Mapper {
-                                        hyperast,
-                                        mapping: hyper_diff::matchers::Mapping {
-                                            src_arena: Decompressible {
-                                                hyperast,
-                                                decomp: a,
-                                            },
-                                            dst_arena: Decompressible {
-                                                hyperast,
-                                                decomp: b,
-                                            },
-                                            mappings: c,
-                                        },
-                                    };
-                                    let _mpr = gt::lazy_greedy_bottom_up_matcher::
-                        LazyGreedyBottomUpMatcher::<_, M, MAX_SIZE>::match_it(mpr);
-                                });
-                            (owned, mappings)
-                            // let mut mapper = Mapper::prep(hyperast, mappings, owned);
-                            // let mpr = mapper.mut_decompressible();
-                            // let _mpr = match mpr { $mpr => {$matcher}};
-                            // mapper.mapping.map(|a| a.decomp, |a| a.decomp)
-                        },
-                    );
-                },
-                name: "aaa".to_owned(),
-                prep: Prep::CD,
-            };
         routine!("LazyGreedy_{}"; [MAX_SIZE]
             mpr => gt::lazy_greedy_bottom_up_matcher::
                 LazyGreedyBottomUpMatcher::<_, M, MAX_SIZE>::match_it(mpr));

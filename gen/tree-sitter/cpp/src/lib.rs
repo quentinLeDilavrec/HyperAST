da1879bb93f894d1fef206b0c6f9500818128480
quentin Le Dilavrec <ledilavrecquentin@gmail.com>
2026-07-04 00:20:37 +0200

    remove remains of alt grammar for cpp

--- gen/tree-sitter/cpp/src/lib.rs
+++ gen/tree-sitter/cpp/src/lib.rs
@@ -2,12 +2,8 @@
 //!
 //! wraps tree-sitter-cpp
 
-cfg_if::cfg_if! { if #[cfg(all(feature = "alt_grammar", feature = "types"))] {
-    pub(crate) mod types_alt;
-    pub(crate) use types_alt as types;
-} else if #[cfg(feature = "types")] {
-    pub(crate) mod types;
-}}
+#[cfg(feature = "types")]
+pub(crate) mod types;
 
 #[cfg(feature = "types")]
 pub use types::{Lang, Role, TIdN, TStore, Type};
@@ -28,20 +24,12 @@ cfg_if::cfg_if! { if #[cfg(feature = "legion")] {
 
 #[cfg(feature = "ts")]
 pub fn language() -> tree_sitter::Language {
-    cfg_if::cfg_if! { if #[cfg(feature = "alt_grammar")] {
-        tree_sitter::Language::new(tree_sitter_cpp_alt::LANGUAGE)
-    } else {
-        tree_sitter::Language::new(tree_sitter_cpp::LANGUAGE)
-    }}
+    tree_sitter::Language::new(tree_sitter_cpp::LANGUAGE)
 }
 
 #[cfg(feature = "ts")]
 pub fn node_types() -> &'static str {
-    cfg_if::cfg_if! { if #[cfg(feature = "alt_grammar")] {
-        tree_sitter_cpp_alt::NODE_TYPES
-    } else {
-        tree_sitter_cpp::NODE_TYPES
-    }}
+    tree_sitter_cpp::NODE_TYPES
 }
 
 #[cfg(all(test, feature = "impl_intern"))]
